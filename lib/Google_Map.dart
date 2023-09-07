0579a17a54275ebce88b1884136b77834796e3ad
tranquanghuy123 <130018198+tranquanghuy123@users.noreply.github.com>
2023-09-07 22:00:24 +0700

    Search APIs google

--- lib/Google_Map.dart
+++ lib/Google_Map.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_api_headers/google_api_headers.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
 
 class GoogleMapSample extends StatefulWidget {
   const GoogleMapSample({super.key});
@@ -13,7 +16,13 @@ class GoogleMapSample extends StatefulWidget {
 class _GoogleMapSampleState extends State<GoogleMapSample> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(10.2556961, 105.9667101);
+  String googleApikey = "GOOGLE_MAP_API_KEY";
+
+  CameraPosition? cameraPosition;
+
+  String location = "Search Location";
+
+  final LatLng startLocation = const LatLng(10.2556961, 105.9667101);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -21,34 +30,97 @@ class _GoogleMapSampleState extends State<GoogleMapSample> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green[700],
-      ),
-      home: Scaffold(
+    return  Scaffold(
         appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          elevation: 2,
+          title: Text("Google Map"),
+          backgroundColor: Colors.deepPurpleAccent,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-          markers: {
-            Marker(
-              markerId: MarkerId('Vĩnh Long'),
-              position: const LatLng(10.2556961, 105.9667101),
-              infoWindow: InfoWindow(
-                title: "Quảng trường tp Vĩnh Long",
+        body: Stack(
+            children:[
 
+              GoogleMap( //Map widget from google_maps_flutter package
+                markers: {
+                  const Marker(
+                    markerId: MarkerId('Vĩnh Long'),
+                    position: LatLng(10.2556961, 105.9667101),
+                    infoWindow: InfoWindow(
+                      title: "Quảng trường tp Vĩnh Long",
+                    ),
+                  ),
+                },
+                zoomGesturesEnabled: true, //enable Zoom in, out on map
+                initialCameraPosition: CameraPosition( //innital position in map
+                  target: startLocation, //initial position
+                  zoom: 14.0, //initial zoom level
+                ),
+                mapType: MapType.normal, //map type
+                onMapCreated: (controller) { //method called when map is created
+                  setState(() {
+                    mapController = controller;
+                  });
+                },
               ),
-            ),
-          },
-        ),
-      ),
+
+
+
+              //search autoconplete input
+              Positioned(  //search input bar
+                  top:10,
+                  child: InkWell(
+                      onTap: () async {
+                        var place = await PlacesAutocomplete.show(
+                            context: context,
+                            apiKey: googleApikey,
+                            mode: Mode.overlay,
+                            types: [],
+                            strictbounds: false,
+                            components: [Component(Component.country, 'np')],
+                            //google_map_webservice package
+                            onError: (err){
+                              print(err);
+                            }
+                        );
+
+                        if(place != null){
+                          setState(() {
+                            location = place.description.toString();
+                          });
+
+                          //form google_maps_webservice package
+                          final plist = GoogleMapsPlaces(apiKey:googleApikey,
+                            apiHeaders: await GoogleApiHeaders().getHeaders(),
+                            //from google_api_headers package
+                          );
+                          String placeid = place.placeId ?? "0";
+                          final detail = await plist.getDetailsByPlaceId(placeid);
+                          final geometry = detail.result.geometry!;
+                          final lat = geometry.location.lat;
+                          final lang = geometry.location.lng;
+                          var newlatlang = LatLng(lat, lang);
+                          //move map camera to selected place with animation
+                          mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: newlatlang, zoom: 17)));
+                        }
+                      },
+                      child:Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Card(
+                          child: Container(
+                              padding: EdgeInsets.all(0),
+                              width: MediaQuery.of(context).size.width - 40,
+                              child: ListTile(
+                                title:Text(location, style: TextStyle(fontSize: 18),),
+                                trailing: Icon(Icons.search),
+                                dense: true,
+                              )
+                          ),
+                        ),
+                      )
+                  )
+              )
+
+
+            ]
+        )
     );
   }
 }
